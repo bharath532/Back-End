6a61882f231fa056a76d6ebbc848fbc7cb4f6c6f
bharath532 <bharath978930@gmail.com>
2025-05-20 07:41:52 +0530

    added

---
+++ file system/index.js
@@ -0,0 +1,47 @@
+const fs = require('fs')
+
+// create new
+// fs.mkdir("Surya",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     else{
+//         console.log("Created.. foldder");
+        
+//     }
+
+// })
+
+// write
+// fs.writeFile('./a.txt',"HEllo BHARATH...",(err)=>{
+//     if(err){
+//         console.log(err);        
+//     }
+//     else{
+//         console.log("File Created...");
+        
+//     }
+// })
+
+// read
+// fs.readFile('./a.txt',(err,data)=>{
+//     if(err){
+//         console.log(err);        
+//     }
+//     else{
+//         console.log(data.toString());
+        
+//     }
+// })
+
+
+// delete
+// fs.unlink('./surya',(err)=>{
+//     if(err){
+//         console.log(err);        
+//     }
+//     else{
+//         console.log("dleted...");
+        
+//     }
+// })
